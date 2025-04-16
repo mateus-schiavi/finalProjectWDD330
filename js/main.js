238e0ea498130a1ff8bf29b68b0d9839f7b6f305
Mateus de Sousa Schiavi <mtsschiavi@gmail.com>
2025-04-16 12:53:09 -0300

    localStorage added

--- js/main.js
+++ js/main.js
@@ -3,11 +3,16 @@
 import { fetchWeather, fetchForecast, fetchCountryInfo } from './api.js';
 import { renderCurrentWeather, renderForecast } from './ui.js';
 
-document.getElementById('searchBtn').addEventListener('click', async () => {
-  const city = document.getElementById('cityInput').value.trim();
+const cityInput = document.getElementById('cityInput');
+const searchBtn = document.getElementById('searchBtn');
+
+searchBtn.addEventListener('click', async () => {
+  const city = cityInput.value.trim();
   if (!city) return;
 
   try {
+    localStorage.setItem('lastCity', city);
+
     const weatherData = await fetchWeather(city);
     const forecastData = await fetchForecast(city);
     const countryData = await fetchCountryInfo(weatherData.sys.country);
@@ -16,5 +21,23 @@ document.getElementById('searchBtn').addEventListener('click', async () => {
     renderForecast(forecastData);
   } catch (err) {
     alert(err.message);
+  }
+});
+
+window.addEventListener('load', async () => {
+  const lastCity = localStorage.getItem('lastCity');
+  if (lastCity) {
+    cityInput.value = lastCity;
+
+    try {
+      const weatherData = await fetchWeather(lastCity);
+      const forecastData = await fetchForecast(lastCity);
+      const countryData = await fetchCountryInfo(weatherData.sys.country);
+
+      renderCurrentWeather(weatherData, countryData);
+      renderForecast(forecastData);
+    } catch (err) {
+      alert(err.message);
+    }
   }
 });
